16ce994ce23fe58068d798d9ea611909167234c7
Anton Varonin <anton_voronin_2011@mail.ru>
2021-03-10 20:12:41 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  let brackets = bracketsConfig.map(x => { return x[0] + x[1] })
+
+  for (let i = 0; i < brackets.length; i++) {
+    if(str.includes(brackets[i])){
+      str = str.replace(brackets[i],'')
+      i= -1
+    }
+
+  }
+
+  return (str.length == 0)
 }
